d25e4c6c4fcfc3728a16fe2a60d762b5f07ac255
Mau Trigo <mtrigo143@gmail.com>
2016-07-21 17:37:55 -0400

    first commit

---
+++ app/Http/Controllers/MiControlador.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class MiControlador extends Controller
+{
+    public function abc(){
+    	return "abc";
+    }
+}
